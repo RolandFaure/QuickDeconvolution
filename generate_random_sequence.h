fb1729a5084c1833b39bbe4e6e2d0aed5a5c6727
Roland Faure <rfaure@nosor.irisa.fr>
2022-05-06 12:05:04 +0200

    Reorganised the files and suppressed useless remnants

--- generate_random_sequence.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-std::string uniform_random_sequence(int length, char alphabet[], int sizeAlphabet);
-
-void chromosomes_to_file(int length, char alphabet[], int sizeAlphabet, int nbChr, std::string fileOut);
